5104fc1e9c2b9c30746c777e8766ee9e0ffdda0d
Caroline Gard <caroline.gard@schibsted.com@a125749.lan>
2023-04-06 10:06:40 +0200

    fix: update package.json

--- docs/alert/Example.vue
+++ docs/alert/Example.vue
@@ -1,7 +1,7 @@
 <script setup>
-    import { wAlert } from '@warp-ds/vue';
-    
-    const showAlert = true;
+  import { wAlert } from '@warp-ds/vue';
+
+  const showAlert = true;
 </script>
 
 <template>
@@ -10,15 +10,15 @@
     <p>I am an excellent message for the user.</p>
   </w-alert>
   <h3>Positive</h3>
-  <w-alert  v-model="showAlert" positive info title="This is the positive variant of the alert element">
+  <w-alert v-model="showAlert" positive info title="This is the positive variant of the alert element">
     <p>With an additional description</p>
   </w-alert>
   <h3>Negative</h3>
-  <w-alert  v-model="showAlert" negative title="This is the negative variant of the alert element">
+  <w-alert v-model="showAlert" negative title="This is the negative variant of the alert element">
     <p>With an additional description</p>
   </w-alert>
   <h3>Warning</h3>
-  <w-alert  v-model="showAlert" warning title="This is the warning variant of the alert element">
+  <w-alert v-model="showAlert" warning title="This is the warning variant of the alert element">
     <p>With an additional description</p>
   </w-alert>
 </template>
