831d06561edb64c51fa91738571c7a688a6f21bb
Anna Rybkina <anna.rybkina@anicode.se>
2023-04-11 09:17:48 +0200

    fix: bump packages

--- docs/buttons/Example.vue
+++ docs/buttons/Example.vue
@@ -5,10 +5,11 @@
 <template>
     <div class="space-x-8">
         <w-button primary>Click me</w-button>
+        <w-button primary loading>Click me</w-button>
         <w-button>Click me</w-button>
         <w-button secondary quiet>Click me</w-button>
         <w-button secondary quiet small>Click me</w-button>
         <w-button primary negative>Click me</w-button>
-        <w-button primary loading>Click me</w-button>
+        <w-button negative quiet>Click me</w-button>
     </div>
 </template>
