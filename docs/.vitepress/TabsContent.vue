f157626b558f7fd0e9ddfff51d2d557eccf4ce17
AnnaRybkina <37986637+AnnaRybkina@users.noreply.github.com>
2023-04-14 16:22:41 +0200

    fix tabs set up a little bit (#11)

--- docs/.vitepress/TabsContent.vue
+++ docs/.vitepress/TabsContent.vue
@@ -1,27 +1,24 @@
 <script setup>
-import Tabs from '../.vitepress/Tabs.vue';
-import Vue from './vue.md';
-import Elements from './elements.md';
-import React from './react.md';
-
+import Tabs from './Tabs.vue';
 const tabs = ['react', 'vue', 'elements'];
+
 </script>
 
 <template>
   <Tabs :tabs="tabs" :initialTab="tabs[0]">
     <template #tab-head-react>React</template>
     <template #tab-panel-react>
-      <react />
+      <slot name="react" />
     </template>
 
     <template #tab-head-vue>Vue</template>
     <template #tab-panel-vue>
-      <vue />
+      <slot name="vue" />
     </template>
 
     <template #tab-head-elements>Elements</template>
     <template #tab-panel-elements>
-      <elements />
+      <slot name="elements" />
     </template>
   </Tabs>
 </template>
