256e48893d436c7fd97966b64aef4a6d3cb1c999
AnnaRybkina <37986637+AnnaRybkina@users.noreply.github.com>
2023-04-14 13:01:43 +0200

    fix: use preflight in the document (#9)

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -3,8 +3,6 @@ import ApiTable from '../ApiTable.vue';
 import ThemeSwitcher from '../ThemeSwitcher.vue';
 import '../bootExamples.js';
 import './custom.css';
-import '@warp-ds/component-classes/common'
-
 
 export default {
   ...DefaultTheme,
