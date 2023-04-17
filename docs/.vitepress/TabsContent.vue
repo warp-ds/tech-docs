955f71c37f8490cf2a9e8187fa33935685fd80d6
AnnaRybkina <37986637+AnnaRybkina@users.noreply.github.com>
2023-04-17 14:29:39 +0200

    fix: tabs refactor (#14)

--- docs/.vitepress/TabsContent.vue
+++ docs/.vitepress/TabsContent.vue
@@ -2,29 +2,19 @@
 import { useSlots } from 'vue';
 import Tabs from './Tabs.vue';
 
-const tabs = ['usage', 'react', 'vue', 'elements'];
 const slots = useSlots();
+const tabs = Object.keys(slots);
+
+const capitalizeTab = (tab) => tab.charAt(0).toUpperCase() + tab.slice(1);
 
 </script>
 
 <template>
   <Tabs :tabs="tabs" :initialTab="tabs[0]">
-      <template v-if="slots.usage" #tab-head-usage>Usage</template>
-      <template v-if="slots.usage" #tab-panel-usage>
-        <slot name="usage" />
-      </template>
-      <template v-if="slots.react" #tab-head-react>React</template>
-      <template v-if="slots.react" #tab-panel-react>
-        <slot name="react" />
-      </template>
-      <template v-if="slots.vue" #tab-head-vue>Vue</template>
-      <template v-if="slots.vue" #tab-panel-vue>
-        <slot name="vue" />
-      </template>
-      <template v-if="slots.elements" #tab-head-elements>Elements</template>
-      <template v-if="slots.elements" #tab-panel-elements>
-        <slot name="elements" />
-      </template>
+    <template v-for="tab in tabs" #[`tab-head-${tab}`]>{{ capitalizeTab(tab) }}</template>
+    <template v-for="tab in tabs" #[`tab-panel-${tab}`]>
+      <slot :name="tab" />
+    </template>
   </Tabs>
 </template>
 
