0ab0ab239df579df0bae720f0ddcc708e15bf695
Snorre Nygaard <snorre.nygaard@finn.no>
2023-04-17 11:33:39 +0200

    fix: move accessability to usage

--- docs/buttons/ButtonTabs.vue
+++ docs/buttons/ButtonTabs.vue
@@ -1,14 +1,24 @@
 <script setup>
 import Tabs from '../.vitepress/Tabs.vue';
-import Vue from './vue.md';
+import Usage from './usage.md'
 import Elements from './elements.md';
 import React from './react.md';
+import Vue from './vue.md';
 
-const tabs = ['react', 'vue', 'elements'];
+const tabs = ['usage','elements','react', 'vue'];
 </script>
 
 <template>
   <Tabs :tabs="tabs" :initialTab="tabs[0]">
+    <template #tab-head-usage>Usage</template>
+    <template #tab-panel-usage>
+      <usage />
+    </template>
+    <template #tab-head-elements>Elements</template>
+    <template #tab-panel-elements>
+      <elements />
+    </template>
+
     <template #tab-head-react>React</template>
     <template #tab-panel-react>
       <react />
@@ -17,11 +27,6 @@ const tabs = ['react', 'vue', 'elements'];
     <template #tab-head-vue>Vue</template>
     <template #tab-panel-vue>
       <vue />
-    </template>
-
-    <template #tab-head-elements>Elements</template>
-    <template #tab-panel-elements>
-      <elements />
     </template>
   </Tabs>
 </template>
