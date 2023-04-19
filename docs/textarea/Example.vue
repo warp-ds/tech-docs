825cb38c4089562a12ea8f0b646db8e17ca10095
Anna Rybkina <anna.rybkina@anicode.se>
2023-04-19 10:58:52 +0200

    fix: add better examples for textfield and textarea

--- docs/textarea/Example.vue
+++ docs/textarea/Example.vue
@@ -4,4 +4,24 @@
 
 <template>
   <w-textarea label="A label" hint="A hint" v-model="model" />
+  <h3>Standard with hint</h3>
+  <div class="component">
+    <w-textarea label="A label" hint="A hint" />
+  </div>
+  <h3>Optional</h3>
+  <div class="component">
+    <w-textarea label="A label" optional />
+  </div>
+  <h3>Disabled</h3>
+  <div class="component">
+    <w-textarea label="A label" disabled />
+  </div>
+  <h3>Read Only</h3>
+  <div class="component">
+    <w-textarea label="A label" value="This is a readOnly textarea" readOnly />
+  </div>
+  <h3>Invalid</h3>
+  <div class="component">
+    <w-textarea label="A label" invalid required />
+  </div>
 </template>
--- docs/textfield/Example.vue
+++ docs/textfield/Example.vue
@@ -1,8 +1,37 @@
 <script setup>
   import { wInput } from '@warp-ds/vue';
+  import { wAffix } from '@warp-ds/vue';
 
 </script>
 
 <template>
-  <w-input label="A label" hint="A hint" v-model="model" />
+  <h3>Standard with hint</h3>
+  <div class="component">
+    <w-input label="A label" hint="A hint" />
+  </div>
+  <h3>Optional with placeholder</h3>
+  <div class="component">
+    <w-input label="A label" placeholder="This is a placeholder" optional />
+  </div>
+  <h3>Disabled</h3>
+  <div class="component">
+    <w-input label="A label" disabled />
+  </div>
+  <h3>Read Only</h3>
+  <div class="component">
+    <w-input label="A label" value="This is a readOnly textfield" readOnly />
+  </div>
+  <h3>Invalid</h3>
+  <div class="component">
+    <w-input label="A label" #suffix invalid required >
+      <w-affix suffix clear />
+    </w-input>
+  </div>
+  <h3>Suffix + Prefix</h3>
+  <div class="component">
+    <w-input label="A label" #prefix #suffix  >
+      <w-affix suffix clear />
+      <w-affix prefix label="kr" />
+    </w-input>
+  </div>
 </template>
