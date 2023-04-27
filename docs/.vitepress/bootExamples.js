f0b93ef9e335fd575a29507ce2f3d5738a6058fe
Snorre Nygaard <snorre.nygaard@finn.no>
2023-04-27 14:20:43 +0200

    Restructure to get ready for more guidelines

--- docs/.vitepress/bootExamples.js
+++ docs/.vitepress/bootExamples.js
@@ -1,10 +1,10 @@
 import { buildWc } from './ex-base.js';
-import Alert from '../alert/Example.vue';
-import Box from '../box/Example.vue';
-import Button from '../buttons/Example.vue';
-import TextArea from '../textarea/Example.vue';
-import Select from '../select/Example.vue';
-import TextField from '../textfield/Example.vue';
+import Alert from '../components/alert/Example.vue';
+import Box from '../components/box/Example.vue';
+import Button from '../components/buttons/Example.vue';
+import TextArea from '../components/textarea/Example.vue';
+import Select from '../components/select/Example.vue';
+import TextField from '../components/textfield/Example.vue';
 
 buildWc('alert-example', Alert);
 buildWc('box-example', Box);
