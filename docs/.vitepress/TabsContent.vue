e8683064a3202353e8a3def1c9e047d876c477cd
AnnaRybkina <37986637+AnnaRybkina@users.noreply.github.com>
2023-05-09 10:28:32 +0200

    Add space to tab title (#35)

--- docs/.vitepress/TabsContent.vue
+++ docs/.vitepress/TabsContent.vue
@@ -5,13 +5,16 @@ import Tabs from './Tabs.vue';
 const slots = useSlots();
 const tabs = Object.keys(slots);
 
-const capitalizeTab = (tab) => tab.charAt(0).toUpperCase() + tab.slice(1);
+const formatTabTitle = (tab) => {
+  const capitalizedTab = tab.charAt(0).toUpperCase() + tab.slice(1);
+  return capitalizedTab?.replaceAll('_', ' ');
+};
 
 </script>
 
 <template>
   <Tabs :tabs="tabs" :initialTab="tabs[0]">
-    <template v-for="tab in tabs" #[`tab-head-${tab}`]>{{ capitalizeTab(tab) }}</template>
+    <template v-for="tab in tabs" #[`tab-head-${tab}`]>{{ formatTabTitle(tab) }}</template>
     <template v-for="tab in tabs" #[`tab-panel-${tab}`]>
       <slot :name="tab" />
     </template>
