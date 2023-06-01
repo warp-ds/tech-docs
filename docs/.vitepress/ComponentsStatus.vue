4b5d1d232cc7eac2779b2edbcabedc9295e9fcfc
CarolineGard <caroline.gard@mpyadigital.com>
2023-06-01 20:54:23 +0200

    feat: add component for showing status

---
+++ docs/.vitepress/ComponentsStatus.vue
@@ -0,0 +1,96 @@
+<script setup>
+import { computed } from 'vue';
+
+// props: 'released' | 'developing' | 'planned' | 'unsupported'
+const props = defineProps({
+  react: {
+    type: String,
+    default: 'unsupported',
+  },
+  vue: {
+    type: String,
+    default: 'unsupported',
+  },
+  elements: {
+    type: String,
+    default: 'unsupported',
+  },
+});
+
+const components = {
+  react: "React",
+  vue: "Vue",
+  elements: "Elements",
+};
+
+const status = {
+  released: "Released",
+  developing: "Developing",
+  planned: "Planned",
+  unsupported: "Not supported",
+}
+
+const reactStatusText = computed( () => status[props.react] );
+const vueStatusText = computed( () => status[props.vue] );
+const elementsStatusText = computed( () => status[props.elements] );
+
+const getStatus = ( key ) => {
+  if ( key === 'react' ) return reactStatusText.value;
+  else if ( key === 'vue' ) return vueStatusText.value;
+  else if ( key === 'elements' ) return elementsStatusText.value;
+  return 'No status'
+}
+</script>
+
+<template>
+  <div class="status">
+    <div v-for="(component, key) in components" :key="key" class="item">
+      <div :class="['circle', `circle-${props[key]}`]"></div>
+      <div>
+        <h4 class="title">{{ component }}</h4>
+        <span>{{ getStatus(key) }}</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.status {
+  display: flex;
+}
+
+.item {
+  display: flex;
+  margin-right: 64px;
+  margin-top: 12px;
+}
+
+.title {
+  margin: 0;
+}
+
+.circle {
+  width: 12px;
+  height: 12px;
+  border-radius: 50%;
+  display: inline-block;
+  margin-right: 12px;
+  margin-top: 8px;
+}
+
+.circle-released {
+  background-color: #67eeb8
+}
+
+.circle-planned {
+  background-color: #66e0ff
+}
+
+.circle-developing {
+  background-color:  #fae76b;
+}
+
+.circle-unsupported {
+  background-color: #f99
+}
+</style>
