4e4602719c07338a3fbf68e422a4c437aae5a1ba
Balbina K <41303231+BalbinaK@users.noreply.github.com>
2023-08-10 13:36:50 +0200

    fix: update button examples (#90)
    * Update button examples
    
    * Update quiet button text and increase vertical gap between btns

--- docs/components/buttons/Example.vue
+++ docs/components/buttons/Example.vue
@@ -3,13 +3,13 @@
 </script>
 
 <template>
-    <div class="space-x-8">
-        <w-button primary>Click me</w-button>
-        <w-button primary loading>Click me</w-button>
-        <w-button>Click me</w-button>
-        <w-button secondary quiet>Click me</w-button>
-        <w-button secondary quiet small>Click me</w-button>
-        <w-button primary negative>Click me</w-button>
-        <w-button negative quiet>Click me</w-button>
+    <div class="space-x-8 space-y-16 text-center">
+        <w-button primary>Primary</w-button>
+        <w-button primary loading>Loading</w-button>
+        <w-button utility>Utility</w-button>
+        <w-button>Secondary</w-button>
+        <w-button secondary quiet>Secondary Quiet</w-button>
+        <w-button primary negative>Negative</w-button>
+        <w-button negative quiet>Negative Quiet</w-button>
     </div>
 </template>
