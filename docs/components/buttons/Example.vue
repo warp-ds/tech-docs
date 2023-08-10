ee9bb02e2c98cfbb87bf8e46ae1baaf62b4d2c30
BalbinaK <balbuhhha@gmail.com>
2023-08-10 13:32:30 +0200

    Update quiet button text and increase vertical gap between btns

--- docs/components/buttons/Example.vue
+++ docs/components/buttons/Example.vue
@@ -3,13 +3,13 @@
 </script>
 
 <template>
-    <div class="space-x-8">
+    <div class="space-x-8 space-y-16 text-center">
         <w-button primary>Primary</w-button>
         <w-button primary loading>Loading</w-button>
         <w-button utility>Utility</w-button>
         <w-button>Secondary</w-button>
-        <w-button secondary quiet>Quiet</w-button>
+        <w-button secondary quiet>Secondary Quiet</w-button>
         <w-button primary negative>Negative</w-button>
-        <w-button negative quiet>Quiet</w-button>
+        <w-button negative quiet>Negative Quiet</w-button>
     </div>
 </template>
