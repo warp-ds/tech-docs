ab1dc34c00a0c14e2ef82820214a7551ef05b7e5
Anna Rybkina <anna.rybkina@anicode.se>
2023-09-22 11:43:28 +0200

    fix: color of the icon name to work in dark mode

--- docs/components/icons/Example.vue
+++ docs/components/icons/Example.vue
@@ -19,14 +19,14 @@
 
 <template>
   <section v-for="(size) in Object.keys(mappedIconsBySize)" style="background-color: var(--vp-c-bg-alt);" class="rounded-8 p-24 mb-24">
-    <h2>{{size}}</h2>
+    <h2 style="color: var(--vp-c-text-1)">{{size}}</h2>
     <main class="max-w-screen-xl mx-auto px-32">
       <div  class="grid gap-24 grid-cols-minmax-100px">
         <div v-for="(icon, fullName) in mappedIconsBySize[size]" :key="fullName">
           <div class="text-center"><div class="mx-auto mb-8 s-bg rounded-4 h-56 flex items-center justify-center flex-col">
             <component v-bind:is="icon" class="s-icon"></component>
           </div>
-          <div class="text-12">{{ getIconName(fullName) }}</div>
+          <p class="text-12" style="color: var(--vp-c-text-1)">{{ getIconName(fullName) }}</p>
         </div>
       </div>
     </div>
