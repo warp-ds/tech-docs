3de5f7365e31320d078bde286decbfa55f070ff0
Magnus Hübner <magnus.hubner@aptitud.se>
2023-09-28 12:24:54 +0200

    Fix broken Select component examples

--- docs/components/select/Example.vue
+++ docs/components/select/Example.vue
@@ -1,12 +1,18 @@
 <script setup>
   import { wSelect } from '@warp-ds/vue';
+  import { ref } from "vue";
+
+  const selectModel1 = ref('');
+  const selectModel2 = ref('');
+  const selectModel3 = ref('');
+  const selectModel4 = ref('');
 </script>
 
 <template>
   <div class="component space-y-16">
     <div>
       <h3 class="h4">Standard with hint and disabled first option</h3>
-      <w-select label="A label" hint="A hint" v-model="model" >
+      <w-select label="A label" hint="A hint" v-model="selectModel1" >
         <option disabled selected value="">Pick something</option>
         <option value="foo">Foo</option>
         <option value="bar">Bar</option>
@@ -14,7 +20,7 @@
     </div>
     <div>
       <h3 class="h4">Optional</h3>
-      <w-select optional v-model="selectModel" label="A useful and informative label">
+      <w-select optional v-model="selectModel2" label="A useful and informative label">
         <option selected value="">Pick something</option>
         <option value="foo">Foo</option>
         <option value="bar">Bar</option>
@@ -22,7 +28,7 @@
     </div>
     <div>
       <h3 class="h4">Disabled</h3>
-      <w-select :disabled="true" v-model="selectModel" label="Disabled select">
+      <w-select :disabled="true" v-model="selectModel3" label="Disabled select">
         <option selected value="">Pick something</option>
         <option value="foo">Foo</option>
         <option value="bar">Bar</option>
@@ -30,7 +36,7 @@
     </div>
     <div>
       <h3 class="h4">Invalid</h3>
-      <w-select required invalid v-model="selectModel" label="Invalid">
+      <w-select required invalid v-model="selectModel4" label="Invalid">
         <option selected value="">Pick something</option>
         <option value="foo">Foo</option>
         <option value="bar">Bar</option>
