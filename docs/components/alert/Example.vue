c00e3826a475ac6242aabe7cb4f6fa036b4cd3dc
BalbinaK <balbuhhha@gmail.com>
2023-10-23 11:40:32 +0200

    docs(alert): set role=status on positive and info variant

--- docs/components/alert/Example.vue
+++ docs/components/alert/Example.vue
@@ -9,13 +9,13 @@
   <div class="component space-y-16">
     <div>
       <h3 class="h4">Info</h3>
-      <w-alert v-model="showAlert" info title="This is the info variant of the alert element">
+      <w-alert v-model="showAlert" info title="This is the info variant of the alert element" role="status">
         <p>I am an excellent message for the user.</p>
       </w-alert>
     </div>
     <div>
       <h3 class="h4">Positive</h3>
-      <w-alert v-model="showAlert" positive title="This is the positive variant of the alert element">
+      <w-alert v-model="showAlert" positive title="This is the positive variant of the alert element" role="status">
         <p>With an additional description</p>
       </w-alert>
     </div>
