80f7d8191ea9a27f0a7d22c534ae81428bb28050
felicia-haggqvist <felicia.haggqvist@schibsted.com>
2023-10-24 15:52:20 +0200

    fix: add a11y-fixes to attention example

--- docs/components/attention/Example.vue
+++ docs/components/attention/Example.vue
@@ -1,6 +1,6 @@
 <script setup>
-import { ref } from 'vue';
-import { wAttention, wBox } from '@warp-ds/vue';
+import { ref } from 'vue'
+import { wAttention, wBox } from '@warp-ds/vue'
 
 const tooltipTarget = ref(null)
 const calloutTarget = ref(null)
@@ -14,26 +14,90 @@ const popoverShowing = ref(false)
 <template>
   <div class="component space-y-16">
     <div>
-      <h3 class="h4">Callout</h3>
+      <h4>Callout</h4>
       <div class="flex items-center">
-        <w-box neutral ref="calloutTarget" @mouseenter="calloutShowing = true; target = $refs.calloutTarget" @mouseleave="calloutShowing = false">Hover over me</w-box>
-        <w-attention callout right :target-el="calloutTarget ? calloutTarget.$el : null" v-model="calloutShowing" class="ml-8">
-          <p>This is a callout</p>
+        <w-box
+          neutral
+          ref="calloutTarget"
+          aria-details="callout-bubbletext"
+          @mouseenter="calloutShowing = true; target = $refs.calloutTarget"
+          @mouseleave="calloutShowing = false"
+          @keydown.escape="calloutShowing = false"
+          @focus="calloutShowing = true"
+          @blur="calloutShowing = false"
+          tabindex="0"
+          >
+          <button>Hover over me</button>
+          </w-box
+        >
+        <w-attention
+          callout
+          right
+          :target-el="calloutTarget ? calloutTarget.$el : null"
+          v-model="calloutShowing"
+          @focus="calloutShowing = true"
+          @blur="calloutShowing = false"
+          class="ml-8"
+        >
+          <p id="callout-bubbletext" role="img">This is a callout</p>
         </w-attention>
       </div>
     </div>
     <div>
-      <h3 class="h4">Tooltip</h3>
-      <w-box neutral ref="tooltipTarget" @mouseenter="tooltipShowing = true; target = $refs.tooltipTarget" @mouseleave="tooltipShowing = false">Hover over me</w-box>
-      <w-attention tooltip bottom :target-el="tooltipTarget ? tooltipTarget.$el : null" v-model="tooltipShowing">
-        <p>This is a tooltip</p>
+      <h4>Tooltip</h4>
+      <w-box
+        neutral
+        ref="tooltipTarget"
+        aria-describedby="tooltip-text"
+        :aria-hidden="!tooltipShowing"
+        @mouseenter=" tooltipShowing = true; target = $refs.tooltipTarget"
+        @mouseleave="tooltipShowing = false"
+        @keydown.escape="tooltipShowing = false"
+        @focus="tooltipShowing = true"
+        @blur="tooltipShowing = false"
+        tabindex="0"
+        >
+        <button>Hover over me</button>
+        </w-box
+      >
+      <w-attention
+        tooltip
+        bottom
+        :target-el="tooltipTarget ? tooltipTarget.$el : null"
+        v-model="tooltipShowing"
+        @focus="tooltipShowing = true"
+        @blur="tooltipShowing = false"
+      >
+        <p id="tooltip-text" role="tooltip">This is a tooltip</p>
       </w-attention>
     </div>
     <div>
-      <h3 class="h4">Popover</h3>
-      <w-box neutral ref="popoverTarget" @mouseenter="popoverShowing = true; target = $refs.popoverTarget" @mouseleave="popoverShowing = false">Hover over me</w-box>
-      <w-attention popover bottom :target-el="popoverTarget ? popoverTarget.$el : null" v-model="popoverShowing">
-        <p>This is a popover</p>
+      <h4>Popover</h4>
+      <w-box
+        neutral
+        ref="popoverTarget"
+        :aria-expanded="popoverShowing"
+        aria-controls="pop-over-attention-example"
+        aria-details="popover-bubbletext"
+        @mouseenter="popoverShowing = true; target = $refs.popoverTarget"
+        @mouseleave="popoverShowing = false"
+        @keydown.escape="popoverShowing = false"
+        @focus="popoverShowing = true"
+        @blur="popoverShowing = false"
+        tabindex="0"
+        >
+        <button>Hover over me</button>
+        </w-box
+      >
+      <w-attention
+        popover
+        bottom
+        :target-el="popoverTarget ? popoverTarget.$el : null"
+        v-model="popoverShowing"
+        @focus="popoverShowing = true"
+        @blur="popoverShowing = false"
+      >
+        <p id="popover-bubbletext" role="img">This is a popover</p>
       </w-attention>
     </div>
   </div>
