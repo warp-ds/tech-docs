ef65c8148aa48298b661ae1a25c61f6c063a5186
BalbinaK <balbuhhha@gmail.com>
2023-10-31 13:20:05 +0100

    Use w-alert web component

--- docs/components/alert/Example.vue
+++ docs/components/alert/Example.vue
@@ -1,34 +1,27 @@
-<script setup>
-  import { wAlert } from '@warp-ds/vue';
-  import { ref } from 'vue';
-
-  const showAlert = ref(true);
-</script>
-
 <template>
   <div class="component space-y-16">
     <div>
       <h3 class="h4">Info</h3>
-      <w-alert v-model="showAlert" info title="This is the info variant of the alert element" role="status">
+      <w-alert show variant="info" role="status">
         <p>I am an excellent message for the user.</p>
       </w-alert>
     </div>
-    <div>
+   <div>
       <h3 class="h4">Positive</h3>
-      <w-alert v-model="showAlert" positive title="This is the positive variant of the alert element" role="status">
-        <p>With an additional description</p>
+      <w-alert show variant="positive" role="status">
+        <p>This is the positive variant of the alert element</p>
       </w-alert>
     </div>
     <div>
       <h3 class="h4">Negative</h3>
-      <w-alert v-model="showAlert" negative title="This is the negative variant of the alert element">
-        <p>With an additional description</p>
+      <w-alert show variant="negative" >
+        <p>This is the negative variant of the alert element</p>
       </w-alert>
     </div>
     <div>
       <h3 class="h4">Warning</h3>
-      <w-alert v-model="showAlert" warning title="This is the warning variant of the alert element">
-        <p>With an additional description</p>
+      <w-alert show variant="warning">
+        <p>This is the warning variant of the alert element</p>
       </w-alert>
     </div>
   </div>
