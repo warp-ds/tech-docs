57b373e319e90ef522a68718a22b1acd913d32c5
BalbinaK <balbuhhha@gmail.com>
2023-10-31 13:21:55 +0100

    use w-button wc

--- docs/components/buttons/Example.vue
+++ docs/components/buttons/Example.vue
@@ -1,16 +1,12 @@
-<script setup>
-  import { wButton } from '@warp-ds/vue';
-</script>
-
 <template>
   <div class="component space-x-8 space-y-16 text-center">
-    <w-button primary>Primary</w-button>
-    <w-button primary loading>Loading</w-button>
-    <w-button utility>Utility</w-button>
+    <w-button variant="primary">Primary</w-button>
+    <w-button variant="primary" loading>Loading</w-button>
+    <w-button variant="utility">Utility</w-button>
     <w-button>Secondary</w-button>
-    <w-button secondary quiet>Secondary Quiet</w-button>
-    <w-button negative>Negative</w-button>
-    <w-button negative quiet>Negative Quiet</w-button>
-    <w-button full-width primary>Primary full width</w-button>
+    <w-button variant="secondary" quiet>Secondary Quiet</w-button>
+    <w-button variant="negative">Negative</w-button>
+    <w-button variant="negative" quiet>Negative Quiet</w-button>
+    <w-button full-width variant="primary">Primary full width</w-button>
   </div>
 </template>
