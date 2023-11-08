e1145a824b071e850f503aa708fde81f106719f5
imprashast <imprashast@gmail.com>
2023-11-08 14:57:06 +0100

    Fix url for post

--- docs/blog/posts.data.js
+++ docs/blog/posts.data.js
@@ -1,16 +1,22 @@
 import { createContentLoader } from "vitepress";
 
+const base = '/tech-docs';
+
 export default createContentLoader("/blog/posts/*.md", {
   includeSrc: true, // include raw markdown source?
   render: true, // include rendered full page HTML?
   excerpt: true, // include excerpt?
   transform(rawData) {
+    console.log(rawData);
     // map, sort, or filter the raw data as you wish.
     // the final result is what will be shipped to the client.
     return rawData
       .sort((a, b) => {
         return +new Date(b.frontmatter.date) - +new Date(a.frontmatter.date);
       })
-      .map((page) => page);
+      .map((page) => ({
+        ...page,
+        url: `${base}${page.url}`,
+    }));
   },
 });
