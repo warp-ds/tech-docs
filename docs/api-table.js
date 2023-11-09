044161cca4333bf8fdbac85455484cac83cd2598
Magnus Hübner <magnuh@users.noreply.github.com>
2023-11-09 13:05:48 +0100

    Add role property to Box component (#123)

--- docs/api-table.js
+++ docs/api-table.js
@@ -99,6 +99,12 @@ export const react = {
         'Will style the box in a neutral fashion',
       ],
       [
+        'role',
+        'string',
+        'region',
+        'Sets the ARIA `role` attribute to provide semantic meaning. To remove the default role attribute, supply an empty role (`role=""`).',
+      ],
+      [
         '~~clickable~~',
         'boolean',
         'false',
@@ -841,6 +847,12 @@ export const vue = {
         'Will style the box in a neutral fashion',
       ],
       [
+        'role',
+        'string',
+        'region',
+        'Sets the ARIA `role` attribute to provide semantic meaning. To remove the default role attribute, supply an empty role (`role=""`).',
+      ],
+      [
         '~~clickable~~',
         'boolean',
         'false',
@@ -1275,6 +1287,12 @@ export const elements = {
         'boolean',
         'false',
         'Will style the box in a neutral fashion',
+      ],
+      [
+        'role',
+        'string',
+        'region',
+        'Sets the ARIA `role` attribute to provide semantic meaning. To remove the default role attribute, supply an empty role (`role=""`).',
       ],
     ],
   },
