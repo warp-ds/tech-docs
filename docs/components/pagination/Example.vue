cd8ea7e70fddf0caabd4e57192a4bbb5bc5eeb57
imprashast <imprashast@gmail.com>
2024-01-12 14:35:07 +0100

    Add Pagination to tech-docs

---
+++ docs/components/pagination/Example.vue
@@ -0,0 +1,25 @@
+<template>
+<div class="component" ref="el" />
+</template>
+
+<script setup>
+import { createElement } from 'react'
+import { createRoot } from 'react-dom/client'
+import { ref, onMounted } from 'vue'
+import { Pagination } from '@warp-ds/react'
+
+const el = ref()
+
+let paginationData = ref({
+    currentPage: 0,
+});
+onMounted(() => {
+  const root = createRoot(el.value)
+  root.render(createElement(Pagination, { currentPage: paginationData.value.currentPage,
+            numPages: 3,
+            lastPage: 3,
+            createHref: (page) => `?page=${page}`,
+            onChange: (page) => paginationData.value.currentPage++ }, null))
+})
+</script>
+
---
+++ docs/components/pagination/index.md
@@ -0,0 +1,15 @@
+<script setup>
+
+</script>
+
+# Pagination
+
+Pagination is used on various listing pages. The current page is indicated by aria-current="page". The component is fully accessible
+
+<components-status react='released' />
+
+## Example
+
+<theme-switcher />
+
+<pagination-example />
