00495253ed6fd28426bee94c450f81bd8dc180cb
Snorre Nygaard <snorrenygaard@hotmail.com>
2024-02-21 09:19:19 +0100

    docs: warp-435 Add Do and Dont boxes for documentation
    * docs: warp-435 Add Do and Dont boxes for documentation
    
    * Merge the Dont component into the Do component by adding a not prop (#180)
    
    * docs: Add a darkmode attribute and a little documentation
    
    ---------
    
    Co-authored-by: Magnus Hübner <magnus.hubner@aptitud.se>

---
+++ docs/.vitepress/DoDont.vue
@@ -0,0 +1,15 @@
+<template>
+  <section class="do-dont-grid">
+    <slot></slot>
+  </section>
+</template>
+
+<style>
+  .do-dont-grid{
+    display: grid;
+    grid-template-columns: repeat(2, 50%);
+    grid-template-rows: auto;
+    grid-gap: 24px;
+    margin-top: 24px;
+  }
+</style>
