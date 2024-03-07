908139d57454d1d2ba84dd2029f5fe98019c448b
BalbinaK <balbuhhha@gmail.com>
2024-03-07 12:08:58 +0100

    Add indeterminte state example and clean up labels

--- docs/components/radiobuttons/Example.vue
+++ docs/components/radiobuttons/Example.vue
@@ -13,15 +13,15 @@ const toggles = [
 </script>
 
 <template>
-  <div class="component">
-    <h3 class="h4">Default</h3>
-    <w-toggle id="radio-button-group" radio-button :equal-width="isJustified" v-model="multiToggleModel" label="Radio-button Toggle" :toggles="toggles" class="mb-32" />
+  <div class="component space-y-16">
+    <h3 class="t4">Default</h3>
+    <w-toggle id="radio-button-group" radio-button :equal-width="isJustified" v-model="multiToggleModel" label="Radio-button Toggle" :toggles="toggles" />
 
-    <h3 class="h4">Small</h3>
-    <w-toggle id="radio-button-group" small radio-button :equal-width="isJustified" v-model="multiToggleModelSmall" label="Radio-button Toggle small" :toggles="toggles" class="mb-32" />
+    <h3 class="t4">Small</h3>
+    <w-toggle id="radio-button-group-small" small radio-button :equal-width="isJustified" v-model="multiToggleModelSmall" label="Small Radio-button Toggle" :toggles="toggles" />
 
     <demo-controls>
-      <w-button aria-controls="radio-button-group" small utility @click="isJustified = !isJustified">{{ isJustified ? 'Unjustify radio button group' : 'Justify radio button group' }}</w-button>
+      <w-button aria-controls="radio-button-group" small utility @click="isJustified = !isJustified">{{ isJustified ? 'Unjustify radio button groups' : 'Justify radio button groups' }}</w-button>
     </demo-controls>
   </div>
 </template>
