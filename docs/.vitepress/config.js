2d47351c6015f6447026bdaecfc67e0f0c03ee41
Snorre Nygaard <snorre.nygaard@finn.no>
2024-05-03 10:47:22 +0200

    Added a very simple explanation on how unocss works and vs tailwind

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -153,6 +153,7 @@ export default defineConfig({
               text: 'Introduction',
               collapsed: true,
               items: [
+                { text: 'unoCSS - whats that?', link: '/classes/unocss' },
                 { text: 'Class Variants', link: '/classes/class-variants' },
                 { text: 'Hover, Focus and other states', link: '/classes/states' },
                 { text: 'Helper classes', link: '/classes/helpers' },
