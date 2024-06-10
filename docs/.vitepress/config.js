d42e252942bb383ce7a6a9f77322b8149275a92c
Magnus Hübner <magnus.hubner@aptitud.se>
2024-06-10 10:52:08 +0200

    Rename Classes menu item and update dependencies

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -226,7 +226,7 @@ export default defineConfig({
           ],
         },
         {
-          text: 'Classes',
+          text: 'CSS',
           collapsed: true,
           items: [
             {
